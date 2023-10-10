72c732755bb44a68e06a34ac49650c537bd34f94
Janardan <jannu0443@gmail.com>
2023-10-10 14:54:50 +0530

    Demo files for GET2 batch

---
+++ AO_Test1.c
@@ -0,0 +1,76 @@
+/*calculating arthematic operation*/
+
+#include<stdio.h>
+int main()
+{
+
+    unsigned int a = 10,b=11;
+
+    b = a^b;
+    printf("output of shift %d\n",b);
+
+     b = b<<1;
+    printf("output of shift %d\n",b);
+
+     b = b>>1;
+    printf("output of shift %d\n",b);
+
+
+   /* unsigned int Data1, Data2, Result = 0;
+
+    printf("Enter Data_1 :\n");
+    scanf("%d",&Data1);
+
+    printf("Enter Data_2 :\n");
+    scanf("%d",&Data2);
+
+    Result = Data1 + Data2;
+    printf("Sum = %d\n",Result);
+
+    Result = Data1 - Data2;
+    printf("Difference = %d\n", Result);
+
+    Result = Data1 * Data2;
+    printf("Product = %d\n", Result);
+
+    Result = Data1 / Data2;
+    printf("Quotient = %d\n", Result);
+
+    Result = Data1 % Data2;
+    printf("Reminder = %d\n", Result);
+
+    printf("PreIncrement = %d\n", ++Data1);
+    printf("PostIncrement = %d\n", Data2++);
+    printf("PostIncrement = %d\n", Data2);*/
+
+
+ /* float Data1, Data2, Result = 0;
+
+
+    printf("Enter Data_1 in Hex format:\n");
+    scanf("%f",&Data1);
+
+    printf("Enter Data_2 in Hex format:\n");
+    scanf("%f",&Data2);
+
+    Result = Data1 + Data2;
+    printf("Sum = %0.2f\n",Result);
+
+    Result = Data1 - Data2;
+    printf("Difference = %0.2f\n", Result);
+
+    Result = Data1 * Data2;
+    printf("Product = %0.2f\n", Result);
+
+    Result = Data1 / Data2;
+    printf("Quotient = %0.2f\n", Result);
+
+    //Result = Data1 % Data2;
+    //printf("Reminder = %0.2f\n", Result);
+
+    printf("PreIncrement = %f\n", ++Data1);
+    printf("PostIncrement = %0.100f\n", Data2++);
+    printf("PostIncrement = %0.10f\n", Data2);
+
+    return 1;*/
+}
